95600a1df290ef1893ffdbcb947b1e463bebbc21
Klarissa Smith <ksmith_email@yahoo.com>
2020-04-03 17:11:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-  new_array.push(array[counter]**=array[counter])
+  array[counter]**
+  new_array.push(array[counter])
   counter += 1
   end
   puts new_array
